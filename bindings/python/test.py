48ae7dd47cb4fdeda64b7ddf293a45a036740f97
QuantumKoalaDev <gruber084@gmail.com>
2025-12-21 19:10:39 +0100

    Clean up python example

--- bindings/python/test.py
+++ bindings/python/test.py
@@ -11,7 +11,6 @@ def main():
     rows = 3
     cols = 3
 
-    # mat = Mat(flat_data, rows, cols)
     mat = Mat.from_flat_array(rows, cols, flat_data)
 
     shape = mat.get_shape()
@@ -35,6 +34,7 @@ def main():
     print(f"Prediction for {new_sample}: {pred}")
 
 def linear_regression_example1():
+    
     # y = 7x - 14
     x_data = [
         0.0,
@@ -45,22 +45,17 @@ def linear_regression_example1():
     ]
     y_data = [-14.0, -7.0, 0.0, 7.0, 14.0]
 
-    # Mat erzeugen: 5 Zeilen, 1 Spalte
-    # mat = Mat(data=x_data, rows=5, cols=1)
     mat = Mat.from_flat_array(5, 1, x_data)
 
-    # Modell erstellen
     model = LinearRegressionModel()
 
     learning_rate = 0.1
     epochs = 500
 
-    # Trainieren
     model.fit(mat, y_data, learning_rate, epochs)
 
-    # Vorhersage
     pred = model.predict([20.0])
-    print(f"Prediction for [20]: {pred}")  # Sollte ~126 sein
+    print(f"Prediction for [20]: {pred}")  # Should be ~126
 
 
 if __name__ == "__main__":
