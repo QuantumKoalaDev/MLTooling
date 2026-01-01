1f961a6c5338fdf9e9a5e81d5608b20b68ab2105
QuantumKoalaDev <gruber084@gmail.com>
2026-01-01 19:20:21 +0100

    Fix DataColumn

--- include/data/DataColumn.hpp
+++ include/data/DataColumn.hpp
@@ -11,10 +11,11 @@ namespace mlt::data
     template <typename T> class DataColumn : public IDataColumn
     {
         std::vector<T> m_data;
+        std::vector<bool> m_validityMask;
 
       public:
-        DataColumn(std::vector<T> data);
-        DataColumn(size_t size);
+        explicit DataColumn(std::vector<T> data);
+        explicit DataColumn(size_t size);
         DataColumn();
         ~DataColumn() override = default;
 
@@ -26,9 +27,12 @@ namespace mlt::data
         DType getType() const noexcept override;
         size_t getSize() const noexcept override;
 
-        void append(T data);
+        void append(T data, bool valid = true);
         void append(std::vector<T> data);
         void append(DataColumn<T> column);
+
+        void remove(size_t row);
+        size_t countNa();
     };
 
     extern template class DataColumn<int64_t>;
--- src/data/DataColumn.cpp
+++ src/data/DataColumn.cpp
@@ -2,6 +2,7 @@
 #include "core/Exceptions.hpp"
 
 #include <stdexcept>
+#include <format>
 
 using namespace mlt::data;
 using namespace mlt::core;
@@ -53,13 +54,14 @@ DType DataColumn<T>::getType() const noexcept
 template <typename T>
 size_t DataColumn<T>::getSize() const noexcept
 {
-	return m_data.size();
+    return m_data.size();
 }
 
 template <typename T>
-void DataColumn<T>::append(T data)
+void DataColumn<T>::append(T data, const bool valid)
 {
-	m_data.push_back(data);
+    m_data.push_back(data);
+    m_validityMask.push_back(valid);
 }
 
 template <typename T>
@@ -72,22 +74,55 @@ void DataColumn<T>::append(std::vector<T> data)
 		std::make_move_iterator(data.begin()),
 		std::make_move_iterator(data.end())
 	);
+
+    m_validityMask.reserve(m_validityMask.size() + data.size());
+
+    for (size_t i = 0; i < data.size(); ++i)
+        m_validityMask.emplace_back(true);
 }
 
 template <typename T>
 void DataColumn<T>::append(DataColumn<T> column)
 {
 	m_data.reserve(m_data.size() + column.m_data.size());
-
 	m_data.insert(
 		m_data.end(),
 		std::make_move_iterator(column.m_data.begin()),
 		std::make_move_iterator(column.m_data.end())
 	);
+
+    m_validityMask.reserve(m_validityMask.size() + column.m_validityMask.size());
+    m_validityMask.insert(
+        m_validityMask.end(),
+        std::make_move_iterator(column.m_validityMask.begin()),
+        std::make_move_iterator(column.m_validityMask.end())
+    );
 }
 
-template class DataColumn<int64_t>;
-template class DataColumn<float>;
-template class DataColumn<double>;
-template class DataColumn<DateTime>;
-template class DataColumn<std::string>;
+template <typename T>
+void DataColumn<T>::remove(size_t row)
+{
+    if (row > m_data.size())
+        throw std::out_of_range(std::format("DataColumn index out of range. (got {}, expected {}",
+            row, m_data.size()));
+
+    using diff_t = std::vector<T>::difference_type;
+    diff_t rowDiff = static_cast<diff_t>(row);
+    m_data.erase(m_data.begin() + rowDiff);
+    m_validityMask.erase(m_validityMask.begin() + rowDiff);
+}
+
+template <typename T>
+size_t DataColumn<T>::countNa()
+{
+    return std::count(m_validityMask.begin(), m_validityMask.end(), false);
+}
+
+namespace mlt::data
+{
+    template class DataColumn<int64_t>;
+    template class DataColumn<float>;
+    template class DataColumn<double>;
+    template class DataColumn<DateTime>;
+    template class DataColumn<std::string>;
+}
