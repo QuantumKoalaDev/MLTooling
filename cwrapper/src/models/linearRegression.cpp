7d98127f67decdd0025ca7ca98ace388dd95eb36
andi <gruber084@gmail.com>
2026-05-11 00:40:49 +0200

    feat(c): Add bindings for linear regression.
    - Implement wrapper for linearRegression class.

---
+++ cwrapper/src/models/linearRegression.cpp
@@ -0,0 +1,69 @@
+#include <mlt/models/linearRegression.h>
+
+#include <mlt/math/Exceptions.hpp>
+#include "linearRegressionInternal.hpp"
+
+#include "vectorInternal.hpp"
+#include "matrixInternal.hpp"
+
+mltStatus mltLinearRegressionCreate(mltLinearRegression **out)
+{
+    if (!out)
+        return MLT_STATUS_ERR_NULL_PTR;
+ 
+    *out = new mltLinearRegression();
+
+    return MLT_STATUS_SUCCESS;
+}
+
+mltStatus mltLinearRegressionDestroy(mltLinearRegression *model)
+{
+    if (!model)
+        return  MLT_STATUS_ERR_NULL_PTR;
+    
+    delete model;
+
+    return MLT_STATUS_SUCCESS;
+}
+
+mltStatus mltLinearRegressionPredict(const mltLinearRegression *model, const mltVectorF *features, float *out)
+{
+    if (!model || !features || !out)
+        return MLT_STATUS_ERR_NULL_PTR;
+    
+    try
+    {
+        *out = model->impl.predict(features->implVec);
+    }
+    catch(const mlt::math::ShapeMismatchException&)
+    {
+        return MLT_STATUS_SHAPE_MISMATCH;
+    }
+    catch (...)
+    {
+        return MLT_STATUS_INTERNAL_ERROR;
+    }
+    
+    return MLT_STATUS_SUCCESS;
+}
+
+mltStatus mltLinearRegressionFit(mltLinearRegression *model, const mltMatrixF *x, const mltVectorF *y, const float learningRate, const size_t epochs)
+{
+    if (!model || !x || !y)
+        return MLT_STATUS_ERR_NULL_PTR;
+
+    try
+    {
+        model->impl.fit(x->implMat, y->implVec, learningRate, epochs);
+    }
+    catch (const mlt::math::ShapeMismatchException&)
+    {
+        return MLT_STATUS_SHAPE_MISMATCH;
+    }
+    catch (...)
+    {
+        return MLT_STATUS_INTERNAL_ERROR;
+    }
+
+    return MLT_STATUS_SUCCESS;
+}
