ba97fe39aa08156cf04bc9ee7f9a7aa772cc413b
andi <gruber084@gmail.com>
2026-06-11 03:25:27 +0200

    Add release build to build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,24 @@
 set -e
 
 BUILD_TYPE=Debug
+BUILD_DIR=build
 
-cmake -S . -B build \
+C_COMPILER=clang
+CPP_COMPILER=clang++
+
+if [[ "$1" == "--release" ]]; then
+
+    BUILD_DIR=build-release
+    BUILD_TYPE=Release
+fi
+
+cmake -S . -B $BUILD_DIR \
     -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+    -DCMAKE_C_COMPILER=$C_COMPILER \
+    -DCMAKE_CXX_COMPILER=$CPP_COMPILER
 
-cmake --build build -j
+cmake --build $BUILD_DIR -j
 
 echo "Done ($BUILD_TYPE)"
+
